42f26f50fe5bb6a9b87fb6388ba08bfe5289162a
Yifan <harry473417@ucla.edu>
2018-11-30 18:30:11 -0800

    added graph cards on the side

--- will.js
+++ will.js
@@ -3,6 +3,7 @@ var qIds = ["screen1", "screen2", "screen3", "screen4", "screen5"];
 //list of all question elements' ids
 
 window.onload = function () {
+    
     initializeQs();
 };
 function getqCurIndex() {
@@ -23,29 +24,41 @@ function yesOrNoBtn(elem, screen) {
     var qCurIndex = getqCurIndex();
 
     let yes = 'yes-' + screen + '-q1'
-    let no = 'no-'+ screen + '-q1'
-    let q2 = screen +'-q2'
+    let no = 'no-' + screen + '-q1'
+    let q2 = screen + '-q2'
+    let card1 = 'card-' + screen + '-q1'
+    let card2 = 'card-' + screen + '-q2'
 
-        //age question
-        //console.log(elem.id == "yes-q1-screen1");
-        if(elem.id == yes) {
-            console.log("hello")
-            elem.style.opacity = 1;
-            document.getElementById(no).style.opacity = 0.2;
+    //console.log(elem.id == "yes-q1-screen1");
+    if (elem.id == yes) {
+        console.log("hello")
+        elem.style.opacity = 1;
+        document.getElementById(no).style.opacity = 0.2;
+        fadeOut(document.getElementById(card1))
+        fadeIn(document.getElementById(q2))
 
-            fadeIn(document.getElementById(q2))
+        //draw new card
+        fadeIn(document.getElementById('card-screen2-q2'))
+        google.charts.load('current', {'packages':['corechart']});
+        google.charts.setOnLoadCallback(drawGraphScreen2Q2);
+        
 
+    }
+    else if (elem.id == no) {
 
-        }
-        else if(elem.id == no) {
+        document.getElementById(yes).style.opacity = 0.2;
+        document.getElementById(no).style.opacity = 1;
+        fadeOut(document.getElementById(q2))
+        fadeOut(document.getElementById(card2))
 
-            document.getElementById(yes).style.opacity = 0.2;
-            document.getElementById(no).style.opacity = 1;
-            fadeOut(document.getElementById(q2))
-        }
-        // console.log("hello")
-        // var input = getYesNoInputUnderQ(qCurIndex)
-        // console.log(input)
+        fadeIn(document.getElementById('card-screen2-q1'))
+        google.charts.load('current', {'packages':['corechart']});
+        google.charts.setOnLoadCallback(drawGraphScreen2Q1);
+        
+    }
+    // console.log("hello")
+    // var input = getYesNoInputUnderQ(qCurIndex)
+    // console.log(input)
 
 }
 
@@ -54,7 +67,7 @@ function threeChoicesBtn(elem) {
     if (qIds[qCurIndex] == "screen2") {
         //age question
         //console.log(elem.id == "yes-q1-screen1");
-        if(elem.id == "spouse-screen2-q2") {
+        if (elem.id == "spouse-screen2-q2") {
             console.log(document.getElementById("no-screen2-q1").style)
             elem.style.opacity = 1;
             document.getElementById("descendants-screen2-q2").style.opacity = 0.2;
@@ -65,14 +78,14 @@ function threeChoicesBtn(elem) {
 
 
         }
-        else if(elem.id == "descendants-screen2-q2") {
+        else if (elem.id == "descendants-screen2-q2") {
             elem.style.opacity = 1;
             document.getElementById("spouse-screen2-q2").style.opacity = 0.2;
             document.getElementById("other-screen2-q2").style.opacity = 0.2;
             //fadeOut(document.getElementById("screen2-q2"))
             fadeOut(document.getElementById("screen2-q3"))
         }
-        else if(elem.id == "other-screen2-q2") {
+        else if (elem.id == "other-screen2-q2") {
             elem.style.opacity = 1;
             document.getElementById("descendants-screen2-q2").style.opacity = 0.2;
             document.getElementById("spouse-screen2-q2").style.opacity = 0.2;
@@ -86,7 +99,7 @@ function threeChoicesBtn(elem) {
     if (qIds[qCurIndex] == "screen3") {
         //age question
         //console.log(elem.id == "yes-q1-screen1");
-        if(elem.id == "spouse-screen3-q2") {
+        if (elem.id == "spouse-screen3-q2") {
             console.log(document.getElementById("no-screen3-q1").style)
             elem.style.opacity = 1;
             document.getElementById("descendants-screen3-q2").style.opacity = 0.2;
@@ -97,14 +110,14 @@ function threeChoicesBtn(elem) {
 
 
         }
-        else if(elem.id == "descendants-screen3-q2") {
+        else if (elem.id == "descendants-screen3-q2") {
             elem.style.opacity = 1;
             document.getElementById("spouse-screen3-q2").style.opacity = 0.2;
             document.getElementById("other-screen3-q2").style.opacity = 0.2;
             //fadeOut(document.getElementById("screen3-q2"))
             fadeOut(document.getElementById("screen3-q3"))
         }
-        else if(elem.id == "other-screen3-q2") {
+        else if (elem.id == "other-screen3-q2") {
             elem.style.opacity = 1;
             document.getElementById("descendants-screen3-q2").style.opacity = 0.2;
             document.getElementById("spouse-screen3-q2").style.opacity = 0.2;
@@ -123,61 +136,6 @@ function nextBtn(elem) {
     exitQ(qCurIndex);
     var qNextIndex = parseInt(qCurIndex) + 1;
 
-
-    if (qIds[qCurIndex] == "q-age") {
-        //age question
-        var age = getNumberInputUnderQ(qCurIndex);
-        sessionStorage.setItem('age', age);
-    }
-    else if (qIds[qCurIndex] == "q-state") {
-        //state question
-        var state = getSelectInputUnderQ(qCurIndex);
-        sessionStorage.setItem('state', state);
-    }
-    else if (qIds[qCurIndex] == "q-money-on-estate-planning") {
-        //money on estate planning question
-        var moneyOnEstatePlanning = getNumberInputUnderQ(qCurIndex);
-        sessionStorage.setItem('money-on-estate-planning', moneyOnEstatePlanning);
-    }
-    else if (qIds[qCurIndex] == 'q-assets-list') {
-        //assets list question
-        var assetsList = getCheckedListInputUnderQ(qCurIndex);
-
-        //retrieve list, store as JSON
-        sessionStorage.setItem("assets-list", JSON.stringify(assetsList));
-    }
-    else if (qIds[qCurIndex] == 'q-marital-status') {
-        //assets list question
-        var maritalStatus = getSelectInputUnderQ(qCurIndex);
-        sessionStorage.setItem('marital-status', maritalStatus);
-        if (maritalStatus == 'engaged' || maritalStatus == 'single') {
-            //if engaged or single, skip over two questions
-            qNextIndex = parseInt(qCurIndex, 10) + 3;
-        }
-    }
-    else if (qIds[qCurIndex] == "q-if-include-kids") {
-        var ifIncludeKids = getRadioButtonInputUnderQ(qCurIndex);
-        sessionStorage.setItem('if-include-kids', ifIncludeKids);
-
-    }
-    else if (qIds[qCurIndex] == "q-if-kids-have-disabilities") {
-        var ifKidsHaveDisabilities = getRadioButtonInputUnderQ(qCurIndex);
-        sessionStorage.setItem('if-kids-have-disabilities', ifKidsHaveDisabilities);
-
-    }
-    else if (qIds[qCurIndex] == 'q-assets-net-worth') {
-        var assetsNetWorth = getRadioButtonInputUnderQ(qCurIndex);
-        sessionStorage.setItem('assets-net-worth', assetsNetWorth);
-
-    }
-    else if (qIds[qCurIndex] == 'q-concerns-list') {
-        //assets list question
-        var concernsList = getCheckedListInputUnderQ(qCurIndex);
-
-        //retrieve list, store as JSON
-        sessionStorage.setItem("concerns-list", JSON.stringify(concernsList));
-
-    }
     if (qNextIndex >= qIds.length) {
         //reached last question, switch to results
         location.href = 'decision_result.html';
@@ -223,20 +181,27 @@ function getRadioButtonInputUnderQ(qCurIndex) {
     var qId = qIds[qCurIndex];
     var radioForms = document.getElementById(qId).getElementsByClassName('active');
     return radioForms[0].children[0].value;
-    for (var i = 0, length = radioForms.length; i < length; i++) {
-        if (radioForms[i].checked) {
-            return radioForms[i].value;
-        }
-    }
-    return "";
+
 }
 function enterQ(qCurIndex) {
     //render in a question
     var qId = qIds[qCurIndex];
     console.log(qId)
+    
     fadeIn(document.getElementById(qId));
+    if(qId == 'screen2'){
+        fadeIn(document.getElementById('card-screen2-q1'))
+        google.charts.load('current', {'packages':['corechart']});
+        google.charts.setOnLoadCallback(drawGraphScreen2Q1);
+    }
+    else if (qId == 'screen3'){
+        fadeIn(document.getElementById('card-screen3-q1'))
+        google.charts.load('current', {'packages':['corechart']});
+        google.charts.setOnLoadCallback(drawGraphScreen3Q1);
+    }
     // document.getElementById(qId).style.display = "block";
 }
+
 function exitQ(qCurIndex) {
     //render out a question
     var qId = qIds[qCurIndex];
@@ -267,4 +232,58 @@ function fadeIn(element) {
         element.style.filter = 'alpha(opacity=' + op * 100 + ")";
         op += op * 0.1;
     }, 20);
+}
+
+
+
+
+///draw functions
+function drawGraphScreen2Q1() {
+    
+    var data = google.visualization.arrayToDataTable([
+    ['category', 'proportion'],
+    ['residence in living trust', 0.5],
+    ['other', 0.5],
+  ]);
+  
+    // Optional; add a title and set the width and height of the chart
+    var options = {'title':'', 'height':300, legend: { position: "none" },};
+  
+    // Display the chart inside the <div> element with id="piechart"
+    var chart = new google.visualization.PieChart(document.getElementById('graph-screen2-q1'));
+    chart.draw(data, options);
+    
+}
+function drawGraphScreen2Q2(){
+    var data = google.visualization.arrayToDataTable([
+        ['Type', "Proportion",  { role: 'style' }],
+        ['of people in first marriages chose Spouse', 0.9, '#b87333'],            // RGB value
+        ['of re-married customers chose My Descendents', 0.9, 'silver'],            // English color name
+        ['of customers do something different', 0.25, 'gold'], // CSS-style declaration
+    ]);
+    
+    var chart = new google.visualization.BarChart(document.getElementById('graph-screen2-q2'));
+    var options = {
+        height:400,
+        legend: { position: "none" },
+    }
+    chart.draw(data, options);
+
+}
+
+function drawGraphScreen3Q1() {
+    
+    var data = google.visualization.arrayToDataTable([
+    ['category', 'proportion'],
+    ['wills that include one of more of these', 0.85],
+    ['other', 0.15],
+  ]);
+  
+    // Optional; add a title and set the width and height of the chart
+    var options = {'title':'', 'height':300, legend: { position: "none" },};
+  
+    // Display the chart inside the <div> element with id="piechart"
+    var chart = new google.visualization.PieChart(document.getElementById('graph-screen3-q1'));
+    chart.draw(data, options);
+    
 }
